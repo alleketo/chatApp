1a5da3b090bbb7d57ea8f22eb493f1d9b7895e88
allek1992 <115907603+allek1992@users.noreply.github.com>
2023-04-06 21:00:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine 
+        height='100vh'
+        projectID='0e397b78-2f28-46ba-a23d-87d52b61bd21'
+        userName='alek'
+        userSecret='qwerty'
+        renderChatFeed = { (chatAppProps)  => <ChatFeed {...chatAppProps } />}
+        onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play }
+      />
     </div>
   );
 }
